73fd2e5d1d48aa891a7c91cd82d2b22be0afd482
yokubjanovich <yokubjanovich@gmail.com>
2023-03-16 16:10:08 +0500

    js da yangilanish

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ let input2 = document.getElementById("input2");
 let input3 = document.getElementById("input3");
 const btn = document.getElementById("btn");
 let body = document.querySelector("body");
+let bollean = true;
 
 btn.addEventListener("click", () => {
-  if (input1.value === input2.value && input1.value === input3.value) {
-    body.style.background = "green";
+  if (bollean == true) {
+    if (
+      input1.value === input2.value &&
+      input1.value === input3.value &&
+      input1.value !== "" &&
+      input2.value !== "" &&
+      input3.value !== ""
+    ) {
+      body.style.background = "green";
+    } else {
+      body.style.background = "red";
+    }
+    bollean = false;
   } else {
-    body.style.background = "red";
+    clear();
   }
 });
+
+function clear() {
+  input1.value = "";
+  input2.value = "";
+  input3.value = "";
+  body.style.background = "#fff";
+  bollean = true;
+}
